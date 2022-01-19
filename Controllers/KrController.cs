925f534efdcc7afd2561d3d57f6df02c81470625
yh9191 <cyh9270@yellowknife.io>
2022-01-19 17:51:55 +0900

    비밀번호 임시발급/마이페이지 작업

--- Controllers/KrController.cs
+++ Controllers/KrController.cs
@@ -63,6 +63,44 @@ namespace K2GGTT.Controllers
 			return View();
 		}
 
+		public IActionResult Password()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult NewPasswordCreate(string email)
+		{
+			Random r = new Random();
+			const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			string code = new string(Enumerable.Repeat(characters, 6).Select(s => s[r.Next(s.Length)]).ToArray());
+
+			var member = _context.Member.Where(x => x.Email == email).FirstOrDefault();
+			if (member == null)
+			{
+				return Content(@"<script type='text/javascript'>alert('가입된 이메일 정보가 없습니다. 다시 확인 바랍니다.');history.back();</script>", "text/html", System.Text.Encoding.UTF8);
+			}
+			member.Password = code;
+			_context.SaveChanges();
+
+			MailMessage mail = new MailMessage();
+			mail.From = new MailAddress("no-reply@yellowknife.io", "K2G", System.Text.Encoding.UTF8);
+			mail.To.Add(email);
+			mail.Subject = "[K2G] 임시 비밀번호가 발급되었습니다.";
+			mail.Body = "임시 비밀번호를 전송해 드립니다. 확인 후 회원정보 수정을 통해 비밀번호를 변경하시기 바랍니다. 비밀번호 : " + code;
+			mail.IsBodyHtml = true;
+			mail.SubjectEncoding = System.Text.Encoding.UTF8;
+			mail.BodyEncoding = System.Text.Encoding.UTF8;
+			SmtpClient SmtpServer = new SmtpClient("email-smtp.ap-northeast-2.amazonaws.com");
+			SmtpServer.Port = 587;
+			SmtpServer.Credentials = new System.Net.NetworkCredential("AKIAUEOAKJWYTC4CMF7U", "BJhoD0cuzQMgK0osvQ6r0vvr9P2SweePd3Cm3V5iz9c2");
+			SmtpServer.EnableSsl = true;
+			SmtpServer.Send(mail);
+			mail.Dispose();
+
+			return Content(@"<script type='text/javascript'>alert('입력하신 이메일 주소로 새로운 비밀번호가 전송되었습니다.');location.href='/Kr/Member';</script>", "text/html", System.Text.Encoding.UTF8);
+		}
+
 		[HttpPost]
 		public IActionResult LoginProc(string memberid, string password)
 		{
@@ -71,6 +109,7 @@ namespace K2GGTT.Controllers
 			{
 				return Content(@"<script type='text/javascript'>alert('로그인 정보가 없습니다.');history.back();</script>", "text/html", System.Text.Encoding.UTF8);
 			}
+			HttpContext.Session.SetString("Id", member.Id.ToString());
 			HttpContext.Session.SetString("MemberId", member.MemberId);
 			HttpContext.Session.SetString("MemberName", member.Name);
 			return Redirect("/Kr/Index");
@@ -89,6 +128,14 @@ namespace K2GGTT.Controllers
 		public IActionResult Myinfo()
 		{
 			MyInfoViewModel model = new MyInfoViewModel();
+			int Id = Convert.ToInt32(HttpContext.Session.GetString("Id"));
+			if (Id > 0)
+			{
+				var member = _context.Member.Where(x => x.Id == Id).FirstOrDefault();
+				model.MemberId = member.MemberId;
+				model.Name = member.Name;
+				model.Contact = member.Contact;
+			}
 			return View(model);
 		}
 

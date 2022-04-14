f0a0ccab7c4fe76eaeb7f847f3240921c07f127f
yh9191 <cyh9270@yellowknife.io>
2022-04-15 08:38:55 +0900

    로그인에 따른 메뉴 visible 처리
    로그인에 따른 메뉴 visible 처리

--- wwwroot/js/common.js
+++ wwwroot/js/common.js
@@ -4,21 +4,21 @@ const color = {
 }
 
 let getParameters = function (paramName) { 
-			var returnValue; 
-			var url = location.href; 
-			var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); 
-			for (var i = 0; i < parameters.length; i++) { 
-				var varName = parameters[i].split('=')[0]; 
-				if (varName.toUpperCase() == paramName.toUpperCase()) { 
-					returnValue = parameters[i].split('=')[1]; 
-					return decodeURIComponent(returnValue); 
-				} 
-			} 
-		};
+	var returnValue; 
+	var url = location.href; 
+	var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); 
+	for (var i = 0; i < parameters.length; i++) { 
+		var varName = parameters[i].split('=')[0]; 
+		if (varName.toUpperCase() == paramName.toUpperCase()) { 
+			returnValue = parameters[i].split('=')[1]; 
+			return decodeURIComponent(returnValue); 
+		}
+	}
+};
 		
 let keywords = getParameters('keywords');
 
-function underconstruction (e) {			
+function underconstruction (e) {
 	if(e == 'en') {alert ('Coming soon.');} else {alert ('준비중입니다.');}
 }
 
